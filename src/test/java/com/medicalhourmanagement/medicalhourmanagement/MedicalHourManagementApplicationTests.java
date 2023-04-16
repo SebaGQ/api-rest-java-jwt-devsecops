d44722b7bfb96fcb24991c98b5fa5af7b0af9f7a
Sebastian Gutierrez <segutierrez.q@gmail.com>
2023-04-16 16:05:57 -0400

    Pendiente: Testing

--- src/test/java/com/medicalhourmanagement/medicalhourmanagement/MedicalHourManagementApplicationTests.java
+++ src/test/java/com/medicalhourmanagement/medicalhourmanagement/MedicalHourManagementApplicationTests.java
@@ -3,6 +3,9 @@ package com.medicalhourmanagement.medicalhourmanagement;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+
+//TODO pendiente hacer el testing
 @SpringBootTest
 class MedicalHourManagementApplicationTests {
 
